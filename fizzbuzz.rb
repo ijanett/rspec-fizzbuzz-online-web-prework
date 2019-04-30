252060ff6aa379bf5ce6d5f6f9ef1bb8a20ee651
Irma J Mejia <ijanettmejia@outlook.com>
2019-04-30 02:01:34 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@ def fizzbuzz(int)
     return "Fizz"
   elsif int % 5 == 0
     return "Buzz"
-  elsif int % 3 == 5 && int % 5 == 3
+  elsif int / 3 == 5 && int / 5 == 3
     return "FizzBuzz"
   else int % 3 != 0 && int % 5 != 0
     puts nil
